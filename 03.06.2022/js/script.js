aef9224002a37191a9e8c448bb33fafbf99873a0
GABRIEL OLIVEIRA DAMASCENO <gabriel.odamasceno@senacsp.edu.br>
2022-06-03 22:28:35 -0300

    Aula 03/06/2022 (Completo) ver1.1

--- 03.06.2022/js/script.js
+++ 03.06.2022/js/script.js
@@ -2,6 +2,11 @@ function moeda(){
     var moeda = document.getElementById("Moeda").value;
     var url = 'https://economia.awesomeapi.com.br/json/last/' + moeda;
     var servico = new XMLHttpRequest(); // XMLHttpRequest é uma classe para Javascript
+    const moedas = () => {
+        USDBRL;
+        EURBRL;
+        BTCBRL;
+    }
 
     servico.open('GET', url); // GET abre o site informado em (var url, url)
 
@@ -11,18 +16,18 @@ function moeda(){
 
     servico.onload = () =>{ // O que acontece depois de carregado (servico.open)
         var resposta = JSON.parse(servico.responseText);
-        var moeda2 = moeda + 'BRL';
-        console.log(moeda2);
+        
+        var chave = Object.keys(resposta);
+        console.log(chave[0]);        
         if(resposta.erro == true){ // Mensagem que vai aparecer quando o CEP estiver inválido
             document.getElementById('Cotação').innerHTML = "Cotação não encontrada"
         }
         else{ // Caso o CEP esteja correto
-            var moeda2 = moeda + 'brl';
             document.getElementById('Cotação').innerHTML =
-            "Codigo da Moeda: " + resposta.moeda2.code + '<br>';
-            // "Nome: " + resposta.moeda2.name + '<br>' +
-            // "Alta: " + resposta.moeda2.high + '<br>' +
-            // "Baixa: " + resposta.moeda2.low;
+            "Codigo da Moeda: " + resposta[chave[0]].code + '<br>' +
+            "Nome: " + resposta[chave[0]].name + '<br>' +
+            "Alta: " + resposta[chave[0]].high + '<br>' +
+            "Baixa: " + resposta[chave[0]].low;
         }   
     }
 
